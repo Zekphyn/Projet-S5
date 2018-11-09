144fa025bd6353439cd309857c3500fff9b118ce
Corentin11 <44088948+Corentin11@users.noreply.github.com>
2018-11-09 15:34:50 +0100

    test inteface graphique

---
+++ src/pendu/Main.java
@@ -0,0 +1,9 @@
+package pendu;
+
+public class Main{
+	public static void main(String[] args){
+		Observable model = new Model();
+		Fenetre fen = new Fenetre(model);
+		fen.setVisible(true);
+	}
+}
