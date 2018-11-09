65b5c7843adf594200241173acc90d0530cba514
AlloucheNejmeddine <allouchenajm@gmail.com>
2018-11-09 15:16:06 +0100

    ajout sudoku

--- src/projet/Sudoku.java
+++ src/projet/Sudoku.java
@@ -1,5 +1,7 @@
 package projet;
 
 public class Sudoku extends Jeu {
+	
+	
 
 }
---
+++ src/sudoku/Position.java
@@ -0,0 +1,22 @@
+package sudoku;
+
+public class Position {
+
+	int ligne ;
+	int colonne;
+	
+	
+	int getLigne() {
+		return ligne;
+	}
+	void setLigne(int ligne) {
+		this.ligne = ligne;
+	}
+	int getColonne() {
+		return colonne;
+	}
+	void setColonne(int colonne) {
+		this.colonne = colonne;
+	}
+	
+}
